379d24c42eef6126b5c6632851fdd6aec70a84ad
aciesielczuk <56974114+aciesielczuk@users.noreply.github.com>
2020-08-28 22:44:20 +0200

    feat: Posts from api

--- src/Posts.js
+++ src/Posts.js
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Posts() {
-    return (
-      <>
-      <p>Dupa</p>
-      </>
-    );
-}
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/posts")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setPosts(result);
+            },
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+          )
+      }, []);
+
+
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {posts.map(p => (
+              <li key={p.id}>
+                {p.id} {p.postBody}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
