3f079ba94c40b7ac20ddf439f2afb84070357899
aciesielczuk <56974114+aciesielczuk@users.noreply.github.com>
2020-08-30 17:29:45 +0200

    feat: posts view has been changed

--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+import './Posts.css'
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
+
+    let token = window.localStorage.getItem("token");
+
+    let addPost = (postBody) => {
+      fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-authorization-token': token,
+            },
+            body: JSON.stringify({
+              postBody: postBody,
+            })
+          }).then(res => res.json())
+            .then(response => {
+              setPosts([...posts, response])
+          });  
+    }
+
+    let handleSubmit = event => {
+      event.preventDefault();
+      addPost(event.target.postBody.value);
+    } 
 
     useEffect(() => {
         fetch("http://localhost:8080/posts", {
@@ -32,13 +61,47 @@ export default function Posts() {
         return <div>Loading...</div>;
       } else {
         return (
+          <>
+          <div classname="container">
+            <div className="col">
+          <Form onSubmit={handleSubmit}>
+          <div className="text-area">
+            <Form.Group controlId="postBody">
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" name="postBody" rows="3"  />
+            </Form.Group>
+          </div>
+            <div className='submit'>
+              <Button type="submit">Add Post</Button>
+            </div>
+          </Form>
+          </div>
           <ul>
             {posts.map(p => (
-              <li key={p.id}>
-                {p.id} {p.postBody}
-              </li>
+              <div className="col">
+              <Card
+                border='primary'
+                key={p.id}
+                style={{ width: '50%', margin: '30px' }}
+                className="card"
+              >
+              <Card.Body className="card-body text-left">
+                <Card.Title className="card-title text-left">
+                  {p.user.username}
+                </Card.Title>
+                  <Card.Text>
+                    {p.postBody}
+                  </Card.Text>
+                <Card.Link href="#">Like</Card.Link>
+                <Card.Link href="#">Add Comment</Card.Link>
+                <Card.Link href="#">Share</Card.Link>
+              </Card.Body>
+              </Card> 
+              </div>           
             ))}
           </ul>
+          </div>
+          </>
         );
       }
     }
